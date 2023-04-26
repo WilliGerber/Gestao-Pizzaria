9cdae5988f2f54dbf3e6e70d93f11fb7d934ff94
WilliGerber <49000944+WilliGerber@users.noreply.github.com>
2023-04-26 16:28:25 -0300

    masks

--- PizzariaZe/Program.cs
+++ PizzariaZe/Program.cs
@@ -45,11 +45,7 @@ namespace PizzariaZe
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            System.Windows.Forms.Application.Run(new Menu());
-
-
-
-            
+            System.Windows.Forms.Application.Run(new Menu());  
         }
     }
 }
